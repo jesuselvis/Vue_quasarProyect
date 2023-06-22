0fc10b2247468a6c903a6e3c44a8aefe9b3e60bb
jesuselvis <jesusmelooficial@gmail.com>
2023-06-22 07:13:45 -0500

    version 1.0

---
+++ pagina-prueba/src/pages/About.vue
@@ -0,0 +1,24 @@
+<template>
+    <q-page padding>
+        <h1>About</h1>
+        <div class="row">
+            <p class="col-12 col-sm-6 col-md-3">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, reiciendis repudiandae quibusdam illum debitis voluptatum nostrum necessitatibus, obcaecati facere sint possimus nihil omnis adipisci quia earum eius magni aliquam est!
+
+            </p>
+            <p class="col-12 col-sm-6 col-md-3">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, reiciendis repudiandae quibusdam illum debitis voluptatum nostrum necessitatibus, obcaecati facere sint possimus nihil omnis adipisci quia earum eius magni aliquam est!
+                
+            </p>
+            <p class="col-12 col-md-6">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam, reiciendis repudiandae quibusdam illum debitis voluptatum nostrum necessitatibus, obcaecati facere sint possimus nihil omnis adipisci quia earum eius magni aliquam est!
+                
+            </p>
+        </div>
+        <div class="row items-center bg-primary text-white text-center" style="height: 400px;">
+            <p class="col bg-positive">lorem.dolor</p>
+            <p class="col bg-negative">lorem.dolor</p>
+            <p class="col bg-info">lorem.dolor</p>
+        </div>
+    </q-page>
+</template>
