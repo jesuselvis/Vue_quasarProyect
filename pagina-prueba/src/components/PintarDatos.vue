b10314cd1c4b1dabea1723cf64babe8e6422dfca
unknown <jesuselvis>
2023-06-23 10:03:20 -0500

    v1.1

---
+++ pagina-prueba/src/components/PintarDatos.vue
@@ -0,0 +1,56 @@
+<template>
+  <p-page>
+    <pre>{{productos}}</pre>
+    
+    <q-table
+      title="Productos en stock"
+      class="q-mt-sm"
+      no-data-label="Tabla vacio, sin datos actuales"
+      :columns="columns"
+      :rows="productos"
+
+    />
+  </p-page>
+</template>
+
+<script>
+
+const columns =[
+  {
+    name: 'producto',
+    label: 'Producto',
+    align: 'left',
+    field: 'producto',
+    sortable: true
+  },
+  {
+    name: 'prioridad',
+    label: 'Prioridad',
+    align: 'left',
+    field: 'prioridad',
+    sortable: true
+  }
+
+]
+
+const rows=[
+   {
+    producto: 'producto 1',
+    prioridad: 'maxima'
+   }
+
+]
+
+export default {
+  // name: 'ComponentName',
+  props:{
+    productos: Array
+  },
+  setup () {
+    return {
+      columns,
+      rows
+    }
+  }
+}
+</script>
--- pagina-prueba/src/pages/Form.vue
+++ pagina-prueba/src/pages/Form.vue
@@ -8,6 +8,7 @@
             class="row q-col-gutter-md"
             @submit.prevent="procesarFormulario"
             @reset="reset"
+            ref="myForm"
         
         > 
 
@@ -15,6 +16,7 @@
                 <q-input
                     label="Producto"
                     v-model="producto"
+                    lazy-rules
                     :rules="[ val => val && val.length > 0 || 'No ingresaste nada.']"
                 />
             </div>
@@ -24,6 +26,7 @@
                     label="Prioridad"
                     v-model="seleccion"
                     :options="opciones"
+                    lazy-rules
                     :rules="[ val => val && val.length > 0 || 'No ingresaste nada.']"
                 />
             </div>
@@ -52,56 +55,75 @@
                 />
             </div>
         </q-form>
-            
+        <pintar-datos :productos="productos"
+        
+        />
     </q-page>
 </template>
 
 <script>
 import {ref} from 'vue'
 import { useQuasar } from 'quasar'
+import PintarDatos from 'src/components/PintarDatos.vue';
+
 export default{
-    setup(){
-        const $q = useQuasar()
-        const producto = ref(null)
-        const seleccion = ref(null)
-        const opciones = ['maxima','moderada','minima']
+    components:{PintarDatos},
+    setup() {
+        const $q = useQuasar();
+        const myForm = ref(null)
+        const producto = ref(null);
+        const seleccion = ref(null);
+        const opciones = ["maxima", "moderada", "minima"];
+
+        const productos = ref([])
 
         const procesarFormulario = () => {
-            console.log("Me diste click al formulario")
-            if (terminos.value === false){
+            console.log("Me diste click al formulario");
+            if (terminos.value === false) {
                 $q.notify({
-                    color: 'red-5',
-                    textColor: 'white',
-                    icon: 'warning',
-                    message: 'Tu no aceptastes los terminos'
-                })
+                    color: "red-5",
+                    textColor: "white",
+                    icon: "warning",
+                    message: "Tu no aceptastes los terminos"
+                });
             }
             else {
                 $q.notify({
-                    color: 'green-4',
-                    textColor: 'white',
-                    icon: 'cloud_done',
-                    message: 'Submitted'
-                })
+                    color: "green-4",
+                    textColor: "white",
+                    icon: "cloud_done",
+                    message: "Submitted"
+                });
+
+                myForm.value.resetValidation()
+        
+                //procesar el formulario
+                productos.value =  [...productos.value,{
+                    producto: producto.value,
+                    prioridad: seleccion.value
+                }]
+                
+                reset()
+
             }
-        }
-
-        const terminos = ref(false)
-
+        };
+        const terminos = ref(false);
         const reset = () => {
-            producto.value = null
-            seleccion.value = null
-            terminos.value = false
-        }
-
-        return{
+            producto.value = null;
+            seleccion.value = null;
+            terminos.value = false;
+        };
+        return {
             producto,
             seleccion,
             opciones,
             procesarFormulario,
             terminos,
-            reset
-        }
-    },
+            reset,
+            productos,
+            myForm
+        };
+    }, 
+    
 }
 </script>
